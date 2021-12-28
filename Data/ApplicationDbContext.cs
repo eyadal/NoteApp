926967279d7e4b7e670309396c6b37099a54e7a7
eyadal <31968847+eyadal@users.noreply.github.com>
2021-12-28 17:28:12 +0100

    created DbContext configuration, and connection string.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteApp.Models;
+
+namespace NoteApp.Data;
+
+    public class ApplicationDbContext :DbContext
+    {
+        // Configure the application DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Create the tables in the Database.
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    // Get the Connection string by the key value
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
