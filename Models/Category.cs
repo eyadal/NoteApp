767d563b54308a85398d4eef3ee380e4e5447d93
eyadal <31968847+eyadal@users.noreply.github.com>
2021-12-28 17:01:56 +0100

    Database tables created with requirements and validation.

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryType { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ Models/note.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteApp.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteBody { get; set; }
+    }
+}
